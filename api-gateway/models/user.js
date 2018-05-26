3940ceb07bf093e3c27bf38971bed1dd1bc0ec52
Matthew Howard <mhoward14@icloud.com>
2018-05-26 17:10:38 -0600

    Adding week-3/

--- api-gateway/models/user.js
+++ api-gateway/models/user.js
@@ -21,3 +21,13 @@
  /**
   Database queries
   */
+
+// user.save is used to add a new user in our database
+module.exports.add = (user, callback) => {
+    user.save(callback);
+};
+
+module.exports.getById = (id, callback) => {
+    var query = {_id: id};
+    User.findById(query, callback);
+}; 
