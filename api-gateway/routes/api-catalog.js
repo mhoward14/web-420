9d2822a6b72a3326754159ce30ee9378437cd97e
Matthew Howard <mhoward14@icloud.com>
2018-06-20 20:18:04 -0600

    Adding week-8/

--- api-gateway/routes/api-catalog.js
+++ api-gateway/routes/api-catalog.js
@@ -4,12 +4,13 @@
 
 var express = require('express');
 var router = express.Router();
+var checkToken = require ('../check-token');
 var auth_controller = require('../controllers/authController');
 //POST Request for registering a user
 router.post('/auth/register', auth_controller.user_register);
 
 //GET Request for verifying user token
-router.get('/auth/token', auth_controller.user_token);
+router.get('/auth/token', checkToken, auth_controller.user_token);
 
 module.exports = router;
 
