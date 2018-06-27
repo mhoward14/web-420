d159c0f84ba81c66fcafa01351ccd3b613805084
Matthew Howard <mhoward14@icloud.com>
2018-06-27 17:10:26 -0600

    Edit to authController

--- api-gateway/controllers/authController.js
+++ api-gateway/controllers/authController.js
@@ -29,7 +29,7 @@ exports.user_register = function(req, res) {
 
 // Verify token on GET
 exports.user_token = function(req, res) {
-    User.getById(decoded.id, function(err, user) {
+    User.getById(req.userId, function(err, user) {
         if (err) return res.status(500).send('There was a problem finding the user.');
 
         if (!user) return res.status(404).send('No user found.');
